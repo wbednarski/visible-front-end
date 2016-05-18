0f98dd32cd7c1b1112feb05bf30c62bf88156a85
Wojciech Bednarski <wbednarski@gmail.com>
2016-05-18 17:30:35 +0200

    Registered User edit feature

--- app/routes/registered-users/registered-user.js
+++ app/routes/registered-users/registered-user.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
+    toggleEditRegisteredUser() {
+      this.controller.toggleProperty('isEditing', true);
+    },
+
+    updateRegisteredUser(model) {
+      model.save();
+      this.send('toggleEditRegisteredUser');
+    },
+
     deleteRegisteredUser(id) {
       let registeredUser = this.store.peekRecord('registered-user', id);
       registeredUser.destroyRecord({}).then(this.transitionTo('registered-users'));
