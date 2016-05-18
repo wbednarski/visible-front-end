8ea064d44c8cb30023d9a2c049ae1de934352437
Wojciech Bednarski <wbednarski@gmail.com>
2016-05-18 14:51:17 +0200

    Registered User - delete single metric feature

--- app/routes/registered-users/registered-user.js
+++ app/routes/registered-users/registered-user.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   actions: {
     deleteRegisteredUser(id) {
       let registeredUser = this.store.peekRecord('registered-user', id);
-      registeredUser.destroyRecord().then(this.transitionTo('registered-users'));
+      registeredUser.destroyRecord({}).then(this.transitionTo('registered-users'));
     }
   }
 });
